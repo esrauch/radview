af3ec84975a5509d82de8810a4a8acc253ef4625
esrauch <esrauch@gmail.com>
2024-08-05 14:12:56 -0400

    tsc

--- out/input/mouse.js
+++ out/input/mouse.js
@@ -5,21 +5,27 @@ export function initMouse() {
     canvas.addEventListener('mousewheel', onMouseWheel);
     canvas.addEventListener('mousemove', onMouseMove);
 }
+function xyForMouseEvent(evt) {
+    return {
+        x: evt.offsetX * devicePixelRatio,
+        y: evt.offsetY * devicePixelRatio
+    };
+}
 function onMouseClick(evt) {
     const e = evt;
-    const center = model.cam.mapInverse({ x: e.offsetX, y: e.offsetY });
+    const center = model.cam.mapInverse(xyForMouseEvent(e));
     console.log(center);
 }
 function onMouseWheel(evt) {
     const e = evt;
-    const center = model.cam.mapInverse({ x: e.offsetX, y: e.offsetY });
+    const center = model.cam.mapInverse(xyForMouseEvent(e));
     model.cam.zoom(1 - e.deltaY / 1000, center);
     evt.preventDefault();
 }
 function onMouseMove(evt) {
     if (evt.buttons == 0)
         return;
-    const delta = model.cam.mapInverseDelta({ x: evt.movementX, y: evt.movementY });
+    const delta = model.cam.mapInverseDelta(xyForMouseEvent(evt));
     model.cam.pan(delta);
     evt.preventDefault();
 }

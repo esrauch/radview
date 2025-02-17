c9bd3d1dab729e7f9355081556266064388fde9e
esrauch <esrauch@gmail.com>
2025-02-17 15:47:21 -0500

    Charts page works

--- out/charts_main.js
+++ out/charts_main.js
@@ -8,6 +8,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import { getHumanActivityName } from "./model/activity.js";
+import { durationSToHHMMSS } from "./util/time.js";
+import { max, min } from "./util/util.js";
 function load() {
     return __awaiter(this, void 0, void 0, function* () {
         const activityJsonFile = yield fetch('rides_json');
@@ -48,20 +50,53 @@ function init(activities) {
         if (!a)
             throw `missing activity ${id}`;
         chartsContainer.innerHTML = '';
+        if (a.latlngs) {
+            const lats = a.latlngs.map(ll => ll[0]);
+            const lons = a.latlngs.map(ll => ll[1]);
+            addChart(lons, lats, 'lon', 'lat');
+        }
         for (const s of a.streams)
-            addChart(a.times, s.data, s.type);
+            addChart(a.times, s.data, 'time', s.type);
     }
-    function addChart(x, y, type) {
-        if (x.length != y.length) {
-            console.error(`Mismatched x/y length for ${type}: ${x.length} vs ${y.length}`);
+    function addChart(xs, ys, xtype, ytype) {
+        if (xs.length != ys.length) {
+            console.error('Mismatched x/y length');
             return;
         }
         const el = document.createElement('div');
         const c = document.createElement('canvas');
         c.width = 800;
         c.height = 600;
+        el.innerHTML = `${xtype} vs ${ytype}<br>`;
+        el.appendChild(c);
+        el.appendChild(document.createElement('hr'));
         const ctx = c.getContext('2d');
-        el.innerHTML = `time vs ${type}<br>`;
+        const minx = min(xs);
+        const miny = min(ys);
+        const maxx = max(xs);
+        const maxy = max(ys);
+        ctx.strokeStyle = '#fff';
+        for (let i = 0; i < xs.length; ++i) {
+            const x = (xs[i] - minx) / (maxx - minx);
+            const y = (ys[i] - miny) / (maxy - miny);
+            ctx.lineTo(x * c.width, (1 - y) * c.height);
+        }
+        ctx.stroke();
+        // y axis labels
+        ctx.fillStyle = '#5da';
+        const numYLabels = 10;
+        for (let i = 1; i < numYLabels; ++i) {
+            const val = (i / numYLabels) * (maxy - miny) + miny;
+            ctx.fillText(val.toFixed(2), 0, (1 - (i / numYLabels)) * c.height);
+        }
+        // x axis labels
+        const numXLabels = 15;
+        for (let i = 1; i < numXLabels; ++i) {
+            const val = (i / numXLabels) * (maxx - minx) + minx;
+            const label = xtype == 'time' ? durationSToHHMMSS(val) : val.toFixed(2);
+            ctx.fillText(label, (i / numXLabels) * c.width, c.height);
+        }
+        chartsContainer.appendChild(el);
     }
 }
 load().then(init);
--- src/charts_main.ts
+++ src/charts_main.ts
@@ -1,4 +1,6 @@
 import { CompactActivity, getHumanActivityName } from "./model/activity.js"
+import { durationSToHHMMSS } from "./util/time.js"
+import { max, min } from "./util/util.js"
 
 async function load(): Promise<CompactActivity[]> {
     const activityJsonFile = await fetch('rides_json')
@@ -39,21 +41,61 @@ function init(activities: CompactActivity[]) {
         const a = activities.find(act => act.id == +id)
         if (!a) throw `missing activity ${id}`
         chartsContainer.innerHTML = ''
-        for (const s of a.streams) addChart(a.times, s.data, s.type)
+
+        if (a.latlngs) {
+            const lats = a.latlngs.map(ll => ll[0])
+            const lons = a.latlngs.map(ll => ll[1])
+            addChart(lons, lats, 'lon', 'lat')
+        }
+
+        for (const s of a.streams) addChart(a.times, s.data, 'time', s.type)
     }
 
-    function addChart(x: number[], y: number[], type: string) {
-        if (x.length != y.length) {
-            console.error(`Mismatched x/y length for ${type}: ${x.length} vs ${y.length}`)
+    function addChart(xs: number[], ys: number[], xtype: string, ytype: string) {
+        if (xs.length != ys.length) {
+            console.error('Mismatched x/y length')
             return
         }
         const el = document.createElement('div')
         const c = document.createElement('canvas')
         c.width = 800
         c.height = 600
-        const ctx = c.getContext('2d')!
+        el.innerHTML = `${xtype} vs ${ytype}<br>`
+        el.appendChild(c)
+        el.appendChild(document.createElement('hr'))
 
-        el.innerHTML = `time vs ${type}<br>`
+        const ctx = c.getContext('2d')!
+        const minx = min(xs)
+        const miny = min(ys)
+        const maxx = max(xs)
+        const maxy = max(ys)
+
+        ctx.strokeStyle = '#fff'
+        for (let i = 0; i < xs.length; ++i) {
+            const x = (xs[i] - minx) / (maxx - minx)
+            const y = (ys[i] - miny) / (maxy - miny)
+            ctx.lineTo(x * c.width, (1 - y) * c.height)
+        }
+        ctx.stroke()
+
+        // y axis labels
+        ctx.fillStyle = '#5da'
+        const numYLabels = 10
+        for (let i = 1; i < numYLabels; ++i) {
+            const val = (i / numYLabels) * (maxy - miny) + miny
+            ctx.fillText(val.toFixed(2), 0, (1 - (i / numYLabels)) * c.height)
+        }
+
+        // x axis labels
+        const numXLabels = 15
+        for (let i = 1; i < numXLabels; ++i) {
+            const val = (i / numXLabels) * (maxx - minx) + minx
+            const label =
+                xtype == 'time' ? durationSToHHMMSS(val) : val.toFixed(2)
+            ctx.fillText(label, (i / numXLabels) * c.width, c.height)
+        }
+
+        chartsContainer.appendChild(el)
     }
 }
 
